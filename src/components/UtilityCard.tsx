35e4cd9df93827272e155c10867f760989afd44d
tempus157 <tempus157@gmail.com>
2022-11-05 04:55:05 +0900

    Make home page layout

---
+++ src/components/UtilityCard.tsx
@@ -0,0 +1,13 @@
+import { Card, CardContent, Typography } from "@mui/material";
+
+const UtilityCard = () => {
+	return (
+		<Card>
+			<CardContent>
+				<Typography>Hello, world!asdfasdfasdfasdfasdfsa</Typography>
+			</CardContent>
+		</Card>
+	);
+};
+
+export default UtilityCard;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,10 @@
 import type { NextPage } from "next";
 import { NextSeo } from "next-seo";
+import { Box, Grid } from "@mui/material";
 import TitleBar from "@/components/TitleBar";
-import { translateInstruction } from "@/libs/translator";
-import { Typography } from "@mui/material";
+import UtilityCard from "@/components/UtilityCard";
 
 const Home: NextPage = () => {
-	const add = translateInstruction("add $t0, $t1, $t2");
-	const addi = translateInstruction("addi $t0, $zero, 16");
-
 	return (
 		<>
 			<NextSeo
@@ -15,8 +12,25 @@ const Home: NextPage = () => {
 				description="MIPS Simulator with a pipeline"
 			/>
 			<TitleBar text="MIPS Simulator" />
-			<Typography>{add.toString(2)}</Typography>
-			<Typography>{addi.toString(2)}</Typography>
+			<Box m="auto" p={1} maxWidth={720}>
+				<Grid container spacing={1} justifyContent="center">
+					<Grid item width="100%">
+						<UtilityCard />
+					</Grid>
+					<Grid item width="100%">
+						<UtilityCard />
+					</Grid>
+					<Grid item width="100%">
+						<UtilityCard />
+					</Grid>
+					<Grid item width="50%">
+						<UtilityCard />
+					</Grid>
+					<Grid item width="50%">
+						<UtilityCard />
+					</Grid>
+				</Grid>
+			</Box>
 		</>
 	);
 };
