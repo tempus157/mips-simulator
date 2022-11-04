3df710bf5a54344399511e4b8459903d02006ffb
tempus157 <tempus157@gmail.com>
2022-11-05 06:15:53 +0900

    Add toolbar icons

--- src/components/Toolbar.tsx
+++ src/components/Toolbar.tsx
@@ -1,12 +1,48 @@
-import { AppBar, Toolbar as AppBarContent, Typography } from "@mui/material";
+import { PlayArrow, SkipNext, Stop, Upload } from "@mui/icons-material";
+import {
+	AppBar,
+	Box,
+	IconButton,
+	Toolbar as AppBarContent,
+	Tooltip,
+} from "@mui/material";
 
 const Toolbar = () => {
 	return (
 		<>
-			<AppBarContent />
+			<Box mt={1}>
+				<AppBarContent />
+			</Box>
 			<AppBar position="fixed" sx={{ top: "auto", bottom: 0 }}>
-				<AppBarContent variant="dense">
-					<Typography>© 2021 Tempus.js</Typography>
+				<AppBarContent>
+					<Box
+						width="100%"
+						display="flex"
+						justifyContent="space-between"
+					>
+						<Tooltip title="Load">
+							<IconButton color="inherit">
+								<Upload />
+							</IconButton>
+						</Tooltip>
+						<Box>
+							<Tooltip title="Reset">
+								<IconButton color="inherit">
+									<Stop />
+								</IconButton>
+							</Tooltip>
+							<Tooltip title="Run">
+								<IconButton color="inherit">
+									<PlayArrow />
+								</IconButton>
+							</Tooltip>
+							<Tooltip title="Step">
+								<IconButton color="inherit">
+									<SkipNext />
+								</IconButton>
+							</Tooltip>
+						</Box>
+					</Box>
 				</AppBarContent>
 			</AppBar>
 		</>
