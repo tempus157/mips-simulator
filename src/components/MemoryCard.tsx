c259714c999edab48a652a4d0a7d52310b1e4007
tempus157 <tempus157@gmail.com>
2022-11-05 05:33:39 +0900

    Make dummy longer

--- src/components/MemoryCard.tsx
+++ src/components/MemoryCard.tsx
@@ -15,6 +15,17 @@ const MemoryCard = () => {
 				<Typography>[00400018] 00000000 nop</Typography>
 				<Typography>[0040001c] 3402000a ori $2, $0, 10</Typography>
 				<Typography>[00400020] 0000000c syscall</Typography>
+				<Typography>[00400000] 8fa40000 lw $4, 0($29)</Typography>
+				<Typography>[00400004] 27a50004 addiu $5, $29, 4</Typography>
+				<Typography>[00400008] 24a60004 addiu $6, $5, 4</Typography>
+				<Typography>[0040000c] 00041080 sll $2, $4, 2</Typography>
+				<Typography>[00400010] 00c23021 addu $6, $6, $2</Typography>
+				<Typography>
+					[00400014] 0c000000 jal 0x00000000 [main]
+				</Typography>
+				<Typography>[00400018] 00000000 nop</Typography>
+				<Typography>[0040001c] 3402000a ori $2, $0, 10</Typography>
+				<Typography>[00400020] 0000000c syscall</Typography>
 			</CardContent>
 		</Card>
 	);
--- src/components/RegisterCard.tsx
+++ src/components/RegisterCard.tsx
@@ -10,6 +10,17 @@ const RegisterCard = () => {
 				<Typography>$2 = 000000</Typography>
 				<Typography>$3 = 000000</Typography>
 				<Typography>$4 = 000000</Typography>
+				<Typography>$5 = 000000</Typography>
+				<Typography>$6 = 000000</Typography>
+				<Typography>$7 = 000000</Typography>
+				<Typography>$8 = 000000</Typography>
+				<Typography>$9 = 000000</Typography>
+				<Typography>$10 = 000000</Typography>
+				<Typography>$11 = 000000</Typography>
+				<Typography>$12 = 000000</Typography>
+				<Typography>$13 = 000000</Typography>
+				<Typography>$14 = 000000</Typography>
+				<Typography>$15 = 000000</Typography>
 			</CardContent>
 		</Card>
 	);
