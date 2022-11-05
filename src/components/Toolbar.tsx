cae4d2a5e3c633e6291ba4e543aafc92bacf5b66
tempus157 <tempus157@gmail.com>
2022-11-05 22:03:35 +0900

    Use flex-grow instead of justify-content

--- src/components/Toolbar.tsx
+++ src/components/Toolbar.tsx
@@ -1,9 +1,9 @@
 import { PlayArrow, SkipNext, Stop, Upload } from "@mui/icons-material";
 import {
 	AppBar,
+	Toolbar as AppBarContent,
 	Box,
 	IconButton,
-	Toolbar as AppBarContent,
 	Tooltip,
 } from "@mui/material";
 
@@ -15,34 +15,27 @@ const Toolbar = () => {
 			</Box>
 			<AppBar position="fixed" sx={{ top: "auto", bottom: 0 }}>
 				<AppBarContent>
-					<Box
-						width="100%"
-						display="flex"
-						justifyContent="space-between"
-					>
-						<Tooltip title="Load">
-							<IconButton color="inherit">
-								<Upload />
-							</IconButton>
-						</Tooltip>
-						<Box>
-							<Tooltip title="Reset">
-								<IconButton color="inherit">
-									<Stop />
-								</IconButton>
-							</Tooltip>
-							<Tooltip title="Run">
-								<IconButton color="inherit">
-									<PlayArrow />
-								</IconButton>
-							</Tooltip>
-							<Tooltip title="Step">
-								<IconButton color="inherit">
-									<SkipNext />
-								</IconButton>
-							</Tooltip>
-						</Box>
-					</Box>
+					<Tooltip title="Load">
+						<IconButton color="inherit">
+							<Upload />
+						</IconButton>
+					</Tooltip>
+					<Box flexGrow={1} />
+					<Tooltip title="Reset">
+						<IconButton color="inherit">
+							<Stop />
+						</IconButton>
+					</Tooltip>
+					<Tooltip title="Run">
+						<IconButton color="inherit">
+							<PlayArrow />
+						</IconButton>
+					</Tooltip>
+					<Tooltip title="Step">
+						<IconButton color="inherit">
+							<SkipNext />
+						</IconButton>
+					</Tooltip>
 				</AppBarContent>
 			</AppBar>
 		</>
