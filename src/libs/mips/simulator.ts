9c509983ba8c120fded99eaac515647a83445b5d
tempus157 <tempus157@gmail.com>
2022-11-07 15:21:52 +0900

    Implement mips.simulator.loadSrc

--- src/libs/mips/simulator.ts
+++ src/libs/mips/simulator.ts
@@ -1,3 +1,5 @@
+import { translateInstruction } from "@/libs/translator";
+
 export interface State {
 	programCounter: number;
 	register: number[];
@@ -5,19 +7,25 @@ export interface State {
 	textMemory: number[];
 }
 
-// TODO: Implement functions
 export const loadSrc = (state: State, src: string): State => {
+	state.textMemory = [];
+	src.split(" ")
+		.map((instructionText) => translateInstruction(instructionText))
+		.forEach((instruction) => state.textMemory.push(instruction));
 	return state;
 };
 
+// TODO: Implement
 export const reset = (state: State): State => {
 	return state;
 };
 
+// TODO: Implement
 export const run = (state: State): State => {
 	return state;
 };
 
+// TODO: Implement
 export const step = (state: State): State => {
 	return state;
 };
